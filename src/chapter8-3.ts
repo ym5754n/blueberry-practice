9364943ed0a61ac2939f60dea55ab14c5813b317
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-04-20 23:08:19 +0900

    chapter8-3

--- src/chapter8-3.ts
+++ src/chapter8-3.ts
@@ -1,3 +1,4 @@
+import { read } from "fs";
 import { readFile } from "fs/promises";
 
 // 8.3.1
@@ -48,4 +49,127 @@ import { readFile } from "fs/promises";
     });
   };
   sleepReject(300).catch(() => console.log("rejected"));
+}
+// 8.3.5
+{
+  const p = Promise.resolve(100);
+  p.then((result) => console.log(result));
+}
+// 8.3.6
+{
+  const pFoo = readFile("foo.txt", "utf-8");
+  const pBar = readFile("foo.txt", "utf-8");
+  const pBaz = readFile("foo.txt", "utf-8");
+
+  const p = Promise.all([pFoo, pBar, pBaz]);
+
+  p.then((results) => {
+    console.log(results[0]);
+    console.log(results[1]);
+    console.log(results[2]);
+  });
+}
+{
+  const p = Promise.all([
+    readFile("foo.txt", "utf-8"),
+    readFile("foo.txt", "utf-8"),
+    readFile("foo.txt", "utf-8"),
+  ]);
+  p.then(([foo, bar, baz]) => {
+    console.log(foo);
+    console.log(bar);
+    console.log(baz);
+  })
+}
+{
+  const sleepReject = (duration: number) => {
+    return new Promise<never>((resolve, reject) => {
+      setTimeout(reject, duration);
+    })
+  };
+  const p = Promise.race([
+    readFile("foo.txt", "utf-8"),
+    sleepReject(5000),
+  ]);
+  p.then((result) => {
+    console.log("success", result);
+  }, (error: unknown) => {
+    console.log("failed", error);
+  });
+}
+// 8.3.7
+{
+  const sleepReject = (duration: number) => {
+    return new Promise<never>((resolve, reject) => {
+      setTimeout(reject, duration);
+    })
+  };
+  const p = Promise.allSettled([
+    readFile("foo.txt", "utf-8"),
+    sleepReject(5000),
+  ]);
+  p.then((result) => console.log(result));
+}
+{
+  const sleepReject = (duration: number) => {
+    return new Promise<never>((resolve, reject) => {
+      setTimeout(reject, duration);
+    })
+  };
+  const p = Promise.any([
+    readFile("foo.txt", "utf-8"),
+    sleepReject(5000),
+  ]);
+  p.then((result) => {
+    console.log(result);
+  });
+}
+// 8.3.8
+{
+  const p = readFile("foo.txt", "utf-8");
+  const p2 = p.then((result) => result + result);
+  p2.then((result) => {
+    console.log(result);
+  });
+}
+{
+  const p = readFile("foo.txt", "utf-8");
+  const p2 = p.catch(() => "");
+  p2.then((result) => {
+    console.log(result);
+  });
+
+  readFile("foo.txt", "utf-8")
+    .finally(() => console.log("foo.txt is loaded?"))
+    .catch(() => "")
+    .then((result) => console.log(result));
+}
+// 8.3.9
+{
+  const repeat10 = (str: string) => {
+    return new Promise<string>((resolve) => {
+      setTimeout(
+        () => resolve(str.repeat(10)),
+        1000
+      );
+    });
+  }
+  readFile("foo.txt", "utf-8")
+    .then((result) => repeat10(result))
+    .then((result) => {
+      console.log(result);
+    });
+}
+// 8.3.11
+{
+  import("fs/promises")
+    .then(({ readFile }) =>
+      readFile("foo.txt", "utf-8")
+    )
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
